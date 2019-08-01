8248d0f6533ae17c4b72d3a87e0d286737cd4651
Paweł Ruta <rutap@tt.com.pl>
2019-08-01 14:22:40 +0200

    db file ready

--- currency exchange.sql
+++ currency exchange.sql
@@ -3,6 +3,8 @@ create table currency (
     short_name varchar(5) not null,
     long_name varchar(50) not null,
     country varchar(50) not null,
+    sell float,
+    buy float,
 	primary key(currency_id)
 );
 
@@ -23,4 +25,19 @@ create table users (
     primary key(user_id),
     foreign key(usrtype_id) references user_types(usrtype_id)
 );
-    
+
+create table transactions (
+	tr_id smallint(4) auto_increment,
+    currency_id smallint(4) not null,
+    tr_date timestamp not null,
+    amount float not null,
+    type smallint(1) not null,
+    user_id smallint(4) not null,
+    client_id smallint(4) not null,
+    info varchar(200),
+    primary key(tr_id),
+    foreign key(currency_id) references currency(currency_id),
+    foreign key(user_id) references users(user_id),
+    foreign key(client_id) references users(user_id)
+);
+
