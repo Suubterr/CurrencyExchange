fabb8ee53192858c72eaf6826dc76f9e2f7bc906
Pawel <rutapoczta@gmail.com>
2019-09-22 21:43:31 +0200

    created Java project, entities ready

--- src/main/java/hibernate/MyCRUD.java
+++ src/main/java/hibernate/MyCRUD.java
@@ -3,37 +3,47 @@ package hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import javax.persistence.Persistence;
 
 public class MyCRUD {
-    public void performCRUDOperation(int operation, SessionFactory sessionFactory, Persistence object) {
+
+    public final String PROPERTIES_FILE_PATH = "src/main/java/hibernate/resources/hibernate.properties";
+
+    private static SessionFactory sf;
+    private static Session session;
 
 
-        Session session = null;
+    public void performCRUDOperation(int operation, Persistence object, Session session) {
+
+
         try {
-            session = sessionFactory.openSession();
+            sf = new Configuration().configure().buildSessionFactory();
+            session = sf.openSession();
 
             switch (operation) {
                 case 1: // create:
-                    System.out.println(operation);
+                    System.out.println(operation + " - create");
                     break;
                 case 2: //read
-                    System.out.println(operation);
+                    System.out.println(operation + " - read");
                     break;
                 case 3: //update
-                    System.out.println(operation);
+                    System.out.println(operation + " - update");
                     break;
                 case 4: //delete
-                    System.out.println(operation);
+                    System.out.println(operation + " - delete");
                     break;
                 default:
                     System.out.println(MyCRUD.class.getName() + ".performCRUDOperation() --> \""+ operation +"\" is not a valid CRUD operation.");
-                    session.close();
-
             }
         } catch (HibernateException e) {
             e.printStackTrace();
+        }  finally {
+            if(session.isOpen()){
+                session.close();
+            }
         }
 
 
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,28 +1,7 @@
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import tools.MyProperties;
-
 import java.io.IOException;
 
 public class test {
     public static void main(String[] args) throws IOException {
-        Configuration cfg = new Configuration();
-        MyProperties properties = new MyProperties();
-        properties.setMyProperties("src/main/java/resources/hibernate.properties");
-        cfg.setProperties(properties.getDbConProp());
-        SessionFactory sf = cfg.buildSessionFactory();
-        Session session = sf.openSession();
-        boolean isConnected = session.isConnected();
-        boolean isOpen = session.isOpen();
 
-        if(isConnected) {
-            System.out.println("Session in connected");
-        }
-
-        if(isOpen) {
-            System.out.println("Session is open");
-            session.close();
-        }
     }
 }
